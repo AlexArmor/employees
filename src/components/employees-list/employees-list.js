baeb23d37ec6465be245c6bbf47b268f6bee7b04
Oleksandr Bron <lexlutermotion@gmail.com>
2023-10-04 17:07:21 +0300

    first commit with settings

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,14 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+import './employees-list.css'
+
+const EmployeesList = ({ data }) => {
+    return (
+        <ul className="app-list list-group">
+            {data.map(item => {
+                return (<EmployeesListItem key={item.name} {...item} />)
+            })}
+        </ul>
+    )
+}
+
+export default EmployeesList;
